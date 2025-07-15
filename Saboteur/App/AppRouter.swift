104b17d19883c5a6a017e3948143e990b6a4fee4
JUDY LEE <ljh.social@ewhain.net>
2025-07-15 10:10:43 +0900

    🔥 Remove. P2P틱택토 타겟이 불필요해짐

---
+++ Saboteur/App/AppRouter.swift
@@ -0,0 +1,8 @@
+//
+//  AppRouter.swift
+//  Saboteur
+//
+//  Created by 이주현 on 7/15/25.
+//
+
+import Foundation
--- Saboteur/Features/GamePlay/GameCoordinator.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct GameCoordinator: View {
-    let onBack: () -> Void
-
-    var body: some View {
-        GameView(onBack: onBack)
-    }
-}
