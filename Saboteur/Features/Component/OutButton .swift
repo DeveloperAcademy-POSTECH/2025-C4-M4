56cb743ee6fe66e647bc4e3d7845d2176ffc0d1a
JUDY LEE <ljh.social@ewhain.net>
2025-07-25 11:04:36 +0900

    ✨ Feat. 비활성화 조건 수정 및 OutButton 컴포넌트 및 에셋 추가

---
+++ Saboteur/Features/Component/OutButton .swift
@@ -0,0 +1,18 @@
+//
+//  OutButton .swift
+//  Saboteur
+//
+//  Created by 이주현 on 7/25/25.
+//
+
+import SwiftUI
+
+struct OutButton_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OutButton_()
+}
