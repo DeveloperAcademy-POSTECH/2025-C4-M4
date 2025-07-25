88d1c26d8e35eac13c625538626da18e2366d105
JUDY LEE <ljh.social@ewhain.net>
2025-07-25 11:05:59 +0900

    ✨ Feat. 비활성화 조건 수정 및 OutButton 컴포넌트 및 에셋 추가

--- Saboteur/Features/Component/OutButton .swift
+++ Saboteur/Features/Component/OutButton .swift
@@ -7,12 +7,60 @@
 
 import SwiftUI
 
-struct OutButton_: View {
+struct OutButton: View {
+    let action: () -> Void
+    let image: Image
+    let activeColor: Color
+    let isDisabled: Bool?
+    var imageWidth: CGFloat?
+
+    init(
+        action: @escaping () -> Void,
+        image: Image,
+        activeColor: Color,
+        isDisabled: Bool? = nil,
+        imageWidth: CGFloat? = nil
+    ) {
+        self.action = action
+        self.image = image
+        self.activeColor = activeColor
+        self.isDisabled = isDisabled
+        self.imageWidth = imageWidth
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: action) {
+            ZStack(alignment: .center) {
+                RoundedRectangle(cornerRadius: 100)
+                    .innerShadow()
+                    .foregroundStyle((isDisabled ?? false) ? Color.Grayscale.gray : activeColor)
+                    .frame(width: 62, height: 50)
+
+                if let width = imageWidth {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: imageWidth)
+                } else {
+                    image
+                }
+            }
+        }
+        .disabled(isDisabled ?? false)
+    }
+}
+
+struct OutButtonPreview: View {
+    var body: some View {
+        OutButton(
+            action: {},
+            image: Image(.xButton),
+            activeColor: Color.Emerald.emerald2,
+            isDisabled: nil
+        )
     }
 }
 
 #Preview {
-    OutButton_()
+    OutButtonPreview()
 }
